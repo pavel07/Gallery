832c6c7aa3d85782509fc125eea98de73240f73f
erickdc <erickson_523@hotmail.com>
2014-08-09 19:06:18 -0600

    Actualizado

---
+++ Galery/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace Galery.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
---
+++ Galery/Models/Galery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Galery.Models
+{
+    public class Galery
+    {
+    }
+}
