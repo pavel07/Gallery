a8738838bd9fe8f3239e59bb32c7981ed2dab8eb
unknown <Pavel-NotePc@.(none)>
2014-08-11 20:14:16 -0600

    Test 3 Ir a Fotos

--- Gallery/App_Start/RouteConfig.cs
+++ Gallery/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Gallery
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Usuario", action = "Index", idGaleria = UrlParameter.Optional }
+                defaults: new { controller = "Usuario", action = "Index", id = UrlParameter.Optional }
             );
 
             routes.MapRoute(
